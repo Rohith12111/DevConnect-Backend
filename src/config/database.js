634fa2d67e5fc7d2b926069aa8d149fa57ec3fb3
Rohith K <krohithreddykuniv@gmail.com>
2025-02-03 15:03:08 -0800

    Changes done

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://krohithreddyk1:jJ2PV7Y1UsS7fXIJ@cluster0.o616m.mongodb.net/devConnect?retryWrites=true&w=majority&appName=Cluster0")                
+    await mongoose.connect("mongodb+srv://krohithreddyk1:JZnWLvHKU1v13xiC@cluster0.o616m.mongodb.net/devConnect?retryWrites=true&w=majority&appName=Cluster0")                
 }
 
 module.exports={
